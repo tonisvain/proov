49f5b09a2c0bf972dc4b1695428fd98af97d8552
kilplane <tonisvain@gmail.com>
2014-10-30 16:37:18 +0200

    project start

---
+++ proov/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for proov project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proov.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
